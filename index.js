b89b749f0639ef3d388bef1f2d75e03c656e40c5
Billouboq <aurelien.boquet.web@gmail.com>
2018-07-15 11:55:44 +0200

    test service and some fixes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 "use strict";
 
 const server = require("./server");
+const config = require("config");
+
+const PORT = config.get("server.port");
 
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 "use strict";
 
 const express = require("express");
-const config = require("config");
 
 const initRoutes = require("./routes");
 const initMiddlewares = require("./middlewares");
 
 const server = express();
-const PORT = config.get("server.port");
 
 initMiddlewares(server);
 initRoutes(server);
